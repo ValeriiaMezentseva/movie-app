377d4eff29c04af8d6d02a27a5d71adf9ff0eb73
Valeriia <mezencevavalerie@gmail.com>
2023-08-16 17:40:34 +0200

    changing the name

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/goit-react-hw-05-movies">
+        <BrowserRouter basename="/movie-app">
           <App />
         </BrowserRouter>
       </PersistGate>
